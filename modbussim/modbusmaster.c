f79ad90c016df2968f4af367a0880f305d61eecf
Whit Schonbein <schonbein@cs.unm.edu>
2016-04-24 20:42:50 -0600

    added ip option to modbusmaster

--- modbussim/modbusmaster.c
+++ modbussim/modbusmaster.c
@@ -24,23 +24,24 @@ int modbus_end(modbus_t *ctx) {
 
 int main(int argc, char **argv) {
 
-    if (argc != 5) {
-        fprintf(stdout, "\nUsage: %s <port> <register address (hex)> <16 bit value (hex)> <freq>\n\n", argv[0]);
+    if (argc != 6) {
+        fprintf(stdout, "\nUsage: %s <ip> <port> <register address (hex)> <16 bit value (hex)> <freq>\n\n", argv[0]);
         return -1;
     }
 
-    int port = atoi(argv[1]);
-    uint16_t address = (uint16_t)strtoul(argv[2], NULL, 16);
-    uint16_t value = (uint16_t)strtoul(argv[3], NULL, 16);
-    int freq = atoi(argv[4]);
+    char *ip = argv[1];
+    int port = atoi(argv[2]);
+    uint16_t address = (uint16_t)strtoul(argv[3], NULL, 16);
+    uint16_t value = (uint16_t)strtoul(argv[4], NULL, 16);
+    int freq = atoi(argv[5]);
     uint16_t *register_values = malloc(sizeof(*register_values));
 
-    printf("value = %X, port = %d, address = %X, freq = $d\n", value, port, address, freq);
+    fprintf(stdout, " ip = %s\n port = %d\n value = %X\n address = %X\n freq = %d\n", ip, port, value, address, freq);
     printf("If freq != 0, ctrl-c to quit\n");
 
-    modbus_t *ctx = modbus_new_tcp(IP, port);
+    modbus_t *ctx = modbus_new_tcp(ip, port);
     if (modbus_connect(ctx) == -1) {
-        fprintf(stderr, "Connection to %s port %d failed: %s\n", IP, port, modbus_strerror(errno));
+        fprintf(stderr, "Connection to %s port %d failed: %s\n", ip, port, modbus_strerror(errno));
         modbus_end(ctx);
         return -1;
     }
